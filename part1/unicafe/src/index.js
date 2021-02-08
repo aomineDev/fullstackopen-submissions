15fb535eff80ae1592c93dfdecf76c92eb114697
aomine <aomine1745@gmail.com>
2021-02-08 18:25:45 -0500

    exercise 1.9 unicafe, step4

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom'
 
 const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
+
+  if (!all) return <p>No feedback given</p>
+
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
   
   return (
     <div>
@@ -10,8 +15,8 @@ const Statistics = ({ good, neutral, bad }) => {
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {all}</p>
-      <p>average {all && (good - bad) / all}</p>
-      <p>positive {all && (good / all) * 100}%</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
@@ -20,7 +25,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
 
   return (
     <div>
