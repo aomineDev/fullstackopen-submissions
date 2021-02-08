fdc934c0e3d2e659efe6401b2bd7c60c08ed7a2a
aomine <aomine1745@gmail.com>
2021-02-08 16:46:15 -0500

    exercise 1.7 unicafe, step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,7 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+
+  const all = good + neutral + bad
+
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -16,6 +18,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {all && (good - bad) / all}</p>
+      <p>positive {all && (good / all) * 100}%</p>
     </div>
   )
 }
